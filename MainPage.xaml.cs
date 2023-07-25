079b53c9ec1d3965b5685f67681a5729839c1b91
Starchm <Starchm@DESKTOP-BKTUL76>
2023-07-25 18:19:53 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace test
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            cv.ItemsSource = new List<string>() { "1","2","3","4"};
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
